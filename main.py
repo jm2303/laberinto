3bf09ded335a7761e7d5062f34cef3b384eee9a2
juan <juanmegu@yahoo.com>
2023-10-30 10:13:28 -0500

    nuevo juego

--- main.py
+++ main.py
@@ -12,15 +12,6 @@ def is_collide(x, y):
     return True
 
 
-def is_game_over():
-    global time, score, record, FPS
-    #if time < 0:
-    #    pygame.time.wait(700)
-    #    player_rect.center = TILE // 2, TILE // 2
-       # set_record(record, score)
-       # record = get_record()
-    #    time, score, FPS = 60, 0, 60
-
 
 
 FPS = 60
@@ -36,8 +27,7 @@ cheese_image = pygame.transform.scale(cheese_image,(TILE  *1.2, TILE*1.2))
 #UI elements
 manager = pygame_gui.UIManager((WIDTH+300, HEIGHT))
 
-# get maze
-maze = generate_maze()
+
 
 # player settings
 player_speed = 1
@@ -45,24 +35,26 @@ player_img = pygame.image.load(r'images\flecha.png').convert()
 player_img.set_colorkey('white') 
 player_img = pygame.transform.scale(player_img, ((TILE ) - 8 , TILE -8))
 player_rect = player_img.get_rect()
-player_rect.center = TILE // 2, TILE // 2
+
 directions = {'a': (-player_speed, 0), 'd': (player_speed, 0), 'w': (0, -player_speed), 's': (0, player_speed)}
 player_angles = {'a': 90, 'd': -90, 'w': 0, 's': 180}
 keys = {'a': pygame.K_LEFT, 'd': pygame.K_RIGHT, 'w': pygame.K_UP, 's': pygame.K_DOWN}
-direction = (0, 0)
-game_starting_time = pygame.time.get_ticks()
-# food settings
 
+ROTATION_TIMER_SEC = 15
+
+player_rect.center = TILE // 2, TILE // 2
+game_starting_time = pygame.time.get_ticks()
+direction = (0, 0)
+last_rotation = pygame.time.get_ticks()
+# get maze
+maze = generate_maze()
 # collision list
 walls_collide_list = sum([cell.get_rects() for cell in maze], [])
-
 # timer
 pygame.time.set_timer(pygame.USEREVENT, 1000)
-score = 0
+#
 game_angle = 0
 player_angle = 0
-ROTATION_TIMER_SEC = 15
-last_rotation = pygame.time.get_ticks()
 
 # fonts
 font = pygame.font.SysFont('Impact', 150)
@@ -74,11 +66,15 @@ def new_game():
     global game_starting_time
     global last_rotation
     global walls_collide_list
+    global game_angle
+    global player_angle
     maze = generate_maze()
     player_rect.center = TILE // 2, TILE // 2
     game_starting_time = pygame.time.get_ticks()
     last_rotation = pygame.time.get_ticks()
     walls_collide_list = sum([cell.get_rects() for cell in maze], [])
+    game_angle = 0
+    player_angle = 0
 
 #GUI elements
 UI_REFRESH_RATE = clock.tick(60)/1000
@@ -91,8 +87,7 @@ button_new = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH + 50,
                                                     object_id='-NEW-')
 
 while True:
-    #surface.blit(bg, (WIDTH, 0))
-    #game_surface.fill('black')
+
     surface.fill('black')
     game_surface.fill('black')
 
@@ -102,8 +97,7 @@ while True:
         if (event.type == pygame_gui.UI_HORIZONTAL_SLIDER_MOVED and
                     event.ui_object_id == '-ROTATION_SLIDER-'):
                     ROTATION_TIMER_SEC = int(slider_input.current_value)
-        if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            #and                event.ui_object_id == '-NEW-'):
+        if event.type == pygame_gui.UI_BUTTON_PRESSED and event.ui_object_id == '-NEW-':
                     new_game()     
 
     manager.process_events(event)
@@ -133,10 +127,8 @@ while True:
     for cell in maze:
         cell.draw(game_surface) 
 
-    # gameplay
-    
-    is_game_over()
 
+    
     # draw player
     rotated_player = pygame.transform.rotate(player_img,player_angle)
     pygame.draw.rect(game_surface,'black',(0,0,TILE-8,TILE))
@@ -148,13 +140,12 @@ while True:
 
 
      # draw stats
-    sec=(  pygame.time.get_ticks() - game_starting_time )/1000
+    sec=(  pygame.time.get_ticks() - game_starting_time )//1000
  
 
     surface.blit(text_font.render('Tiempo', True, pygame.Color('cyan'), True), (WIDTH + 30, 30))
-    #surface.blit(font.render(f'{(pygame.time.get_ticks() - last_rotation)//1000}', True, pygame.Color('cyan')), (WIDTH + 70, 130))
     surface.blit(text_font.render(str(timedelta(seconds=sec)), True, pygame.Color('magenta')), (WIDTH + 50, 130))
-    surface.blit(text_font.render('Rotación (seg)', True, pygame.Color('cyan')), (WIDTH + 30, 230))
+    surface.blit(text_font.render('Rotación', True, pygame.Color('cyan')), (WIDTH + 30, 230))
     manager.draw_ui(surface)
     surface.blit(small_font.render(str(ROTATION_TIMER_SEC), True, pygame.Color('magenta')), (WIDTH + 260, 340))
 
